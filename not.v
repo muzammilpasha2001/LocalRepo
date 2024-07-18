aa8f34b27263adb2f3da955a6ab3acee86badce0
Muzammilpasha2001 <Muzammilpasha28712871@gmail.com>
2024-07-18 07:32:41 +0530

    this includes a testbench

--- not.v
+++ not.v
@@ -3,3 +3,18 @@ input a;
 output out;
 assign out=~a;
 endmodule
+
+
+module tb;
+reg a;
+wire out;
+mynot dut(a,out);
+initial
+begin 
+$monitor($time, "a=%b,out=%b", a,out);
+a=1'b1;
+#10 
+a=1'b0;
+#10
+end 
+endmodule
